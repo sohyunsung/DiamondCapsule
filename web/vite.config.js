9b80a01a90b2822db4bd4eb47b85340a39e1b7eb
sohyunsung <sohyunsung@users.noreply.github.com>
2026-07-21 15:18:58 +0900

    GitHub Pages: vite base 설정

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/DiamondCapsule/", // GitHub Pages 서브경로
   plugins: [react()],
 });
